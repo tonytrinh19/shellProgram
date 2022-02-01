b152ced0db505fc01c72833b3a812a52f4e26760
toni <tony.q.trinh@gmail.com>
2022-01-31 17:40:57 -0800

    Switched to using wordexp

--- src/command.c
+++ src/command.c
@@ -5,11 +5,14 @@
 #include <dc_posix/dc_string.h>
 #include <ctype.h>
 #include <dc_posix/dc_stdlib.h>
+#include <wordexp.h>
 #include "command.h"
 
 void parse_command(const struct dc_posix_env *env, struct dc_error *err,
                    struct state *state, struct command *command)
 {
+    wordexp_t exp;
+    int status;
     char *string;
     regmatch_t match;
     int matched;
@@ -35,6 +38,8 @@ void parse_command(const struct dc_posix_env *env, struct dc_error *err,
     {
         char *str2;
         char *str;
+        wordexp_t exp;
+        int status;
         // Offset of the first character of the stderr file name from the start. " 2>"
         size_t offset = 3;
 
@@ -55,10 +60,17 @@ void parse_command(const struct dc_posix_env *env, struct dc_error *err,
         str2 = dc_malloc(env, err, sizeof(char) * size);
         strncpy(str2, &str[offset], size);
         str2[size] = '\0';
-
-        dc_str_trim(env, str2);
-        dc_expand_path(env, err, &expanded_stderr_file, str2);
-        command->stderr_file = expanded_stderr_file;
+        // wordexp
+        status = wordexp(str2, &exp, 0);
+        if (status == 0)
+        {
+            command->stderr_file = strdup(exp.we_wordv[0]);
+            wordfree(&exp);
+        }
+        else
+        {
+            state->fatal_error = true;
+        }
         free(str2);
         free(str);
     }
@@ -73,6 +85,8 @@ void parse_command(const struct dc_posix_env *env, struct dc_error *err,
     {
         char *str2;
         char *str;
+        wordexp_t exp;
+        int status;
         // Offset of the first character of the stdout file name from the start. " >"
         size_t offset = 2;
 
@@ -94,9 +108,17 @@ void parse_command(const struct dc_posix_env *env, struct dc_error *err,
         strncpy(str2, &str[offset], size);
         str2[size] = '\0';
 
-        dc_str_trim(env, str2);
-        dc_expand_path(env, err, &expanded_stdout_file, str2);
-        command->stdout_file = expanded_stdout_file;
+        // wordexp
+        status = wordexp(str2, &exp, 0);
+        if (status == 0)
+        {
+            command->stdout_file = strdup(exp.we_wordv[0]);
+            wordfree(&exp);
+        }
+        else
+        {
+            state->fatal_error = true;
+        }
         free(str2);
         free(str);
     }
@@ -111,6 +133,8 @@ void parse_command(const struct dc_posix_env *env, struct dc_error *err,
     {
         char *str2;
         char *str;
+        wordexp_t exp;
+        int status;
         // Offset of the first character of the stdin file name from the start. " <"
         size_t offset = 2;
 
@@ -125,59 +149,71 @@ void parse_command(const struct dc_posix_env *env, struct dc_error *err,
         str2 = dc_malloc(env, err, sizeof(char) * size);
         strncpy(str2, &str[offset], size);
         str2[size] = '\0';
-        dc_str_trim(env, str2);
-        dc_expand_path(env, err, &expanded_stdin_file, str2);
-        command->stdin_file = expanded_stdin_file;
+        // wordexp
+        status = wordexp(str2, &exp, 0);
+        if (status == 0)
+        {
+            command->stdin_file = strdup(exp.we_wordv[0]);
+            wordfree(&exp);
+        }
+        else
+        {
+            state->fatal_error = true;
+        }
         free(str2);
         free(str);
     }
-
-    sizeArray = 1;
-    index     = 1;
-    rest = strdup(string);
-    temp = rest;
-
-    // Count number of elements by counting spaces
-    for (size_t i = 0; i < dc_strlen(env, string); ++i)
+    status = wordexp(string, &exp, 0);
+    if (status == 0)
     {
-        if (isspace(string[i])) sizeArray++;
+        command->argc = exp.we_wordc;
+        command->argv = dc_calloc(env, err, (exp.we_wordc + 2), sizeof(char *));
+
+        for (size_t i = 1; i < exp.we_wordc; ++i)
+        {
+            command->argv[i] = strdup(exp.we_wordv[i]);
+        }
+        command->command = strdup(exp.we_wordv[0]);
+        wordfree(&exp);
     }
-
-    // +1 for NULL element at the end
-    argv = dc_calloc(env, err, sizeArray + 1, sizeof(char *));
-
-    if (dc_error_has_error(err))
+    else
     {
         state->fatal_error = true;
     }
 
-    token = strtok_r(rest, " ", &rest);
-    command->command = strdup(token);
-
-    while((token = strtok_r(rest, " ", &rest)))
-    {
-        argv[index] = strdup(token);
-        index++;
-    }
-
-    argv[0]         = NULL;
-    argv[sizeArray] = NULL;
-    command->argv   = argv;
-    command->argc   = sizeArray;
-
-//    free(rest);
     free(string);
-    free(temp);
-    //    char* token;
-//    char* rest = strdup(command->line);
-//    char* temp = rest;
-//
-//    token = strtok_r(rest, " ", &rest);
-//    printf("command: %s\n", token);
-//    command->command = strdup(token);
 }
 
 void destroy_command(const struct dc_posix_env *env, struct command *command)
 {
 
 }
+
+
+//    sizeArray = 1;
+//    index     = 1;
+//    rest = strdup(string);
+//    temp = rest;
+//
+//    // Count number of elements by counting spaces
+//    for (size_t i = 0; i < dc_strlen(env, string); ++i)
+//    {
+//        if (isspace(string[i])) sizeArray++;
+//    }
+//
+//    // +1 for NULL element at the end
+//    argv = dc_calloc(env, err, sizeArray + 1, sizeof(char *));
+//
+//    if (dc_error_has_error(err))
+//    {
+//        state->fatal_error = true;
+//    }
+//
+//    token = strtok_r(rest, " ", &rest);
+//    command->command = strdup(token);
+//
+//    while((token = strtok_r(rest, " ", &rest)))
+//    {
+//        argv[index] = strdup(token);
+//        index++;
+//    }
